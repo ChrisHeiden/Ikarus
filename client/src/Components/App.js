7ec072a75d8b92c544d9be49e742f1faba3f0d5f
Christopher Heiden <s0549453@htw-berlin.de>
2019-07-19 08:35:50 +0200

    add changes

--- client/src/Components/App.js
+++ client/src/Components/App.js
@@ -3,29 +3,16 @@ import '../Style/App.css';
 import Transition from './Transition';
 
 class App extends Component {
-  state = {tweets: []}
-
-  componentDidMount() {
-    fetch('/users')
-      .then(res => res.json())
-      .then(tweet => {
-        for(let i = 0; i < tweet.length; ++i)
-        {
-            console.log(tweet[i].user.location);
-        }   
-        this.setState({tweets: tweet})
-      });
-  };
 
   render() {
     return (
-      <React.Fragment>
-        <div className="title">
-          <h1>Climate</h1>
-          <h1><span/>Change</h1>
+      <div className="generalGrid">
+        <div className="titleGridPos titleFlexLayout">
+          <h1 className="climate">Climate</h1>
+          <h1 className="change"><span/>Change</h1>
         </div>
         <Transition/>
-      </React.Fragment>
+      </div>
     );
   }
 }
