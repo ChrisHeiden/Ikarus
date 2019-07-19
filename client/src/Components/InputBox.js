23966ab1fb9cb5b6c038853bb3da28368fc14651
Christopher Heiden <s0549453@htw-berlin.de>
2019-07-19 08:40:32 +0200

    add InfoBox in a general state

---
+++ client/src/Components/InputBox.js
@@ -0,0 +1,39 @@
+import React, { Component } from 'react';
+import '../Style/InputBox.css';
+
+class InputBox extends Component {
+    
+    constructor(props){
+        super(props);
+        this.state = {
+            sliderValue: 0,
+            textValue: "",            
+        };
+
+        this.handleSlider = this.handleSlider.bind(this);
+        this.handleText = this.handleText.bind(this);
+    } 
+
+    handleSlider(event){
+        this.setState({sliderValue: event.target.value});
+    }
+
+    handleText(event) {
+        this.setState({textValue: event.target.value});
+    }
+
+    render() {
+        return (
+            <div className="InputBox">
+                <p>Amount of Locations</p>
+                <input type="range" min="0" max="100" value={this.state.SliderValue} onChange={this.handleSlider}/>
+
+                <p>Name of the Locations</p>
+                <input  type="text" value={this.state.textValue} onChange={this.handleText}/>
+            </div>
+        );
+    }
+}
+
+//TODO: change max to real Max value that has been found
+export default InputBox;
