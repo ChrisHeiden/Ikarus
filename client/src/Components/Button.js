18763acda5d3d2a2d66b2ecd27dee06320920950
Christopher <s0549453@htw-berlin.de>
2019-07-19 09:37:20 +0200

    change button sytle

--- client/src/Components/Button.js
+++ client/src/Components/Button.js
@@ -6,18 +6,42 @@ class Button extends Component {
   
     constructor(props) {
         super(props);
+        this.state ={
+            height: 0,
+            showBox: false
+        }; 
 
         this.showBox = this.showBox.bind(this);
     }
+    componentDidMount(){
+        console.log(this.refs.button.getBoundingClientRect());
+        console.log(this.refs.button.getBoundingClientRect().width);
+        this.state.height = this.refs.button.getBoundingClientRect().width;
+        this.forceUpdate();
+    }
 
     showBox(){
-        console.log("Hallo");
+        if(this.state.showBox === false)
+        {
+            this.setState({ showBox: true });
+        }
+        else{
+            this.setState({ showBox: false });
+        }
     }
 
     render() {
+        let showBoxComp;
+
+       
+        if(this.state.showBox === true)
+        {
+            showBoxComp = <InputBox/>;
+        };
         return (
         <React.Fragment>
-            <button>{this.props.content}</button>
+            <button style={{height: this.state.height}} ref="button" onClick={this.showBox}>{this.props.content}</button>
+            {showBoxComp}
         </React.Fragment>
         );
   }
@@ -25,4 +49,4 @@ class Button extends Component {
 
 export default Button;
 
-//<InputBox/>
+//  
