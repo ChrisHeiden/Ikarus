ee9590de28900826f330e4ca7455c77b8f0870c4
Christopher <s0549453@htw-berlin.de>
2019-07-20 18:13:30 +0200

    add a new Component

---
+++ client/src/Components/InfoBox.js
@@ -0,0 +1,49 @@
+import React, { Component } from 'react';
+import '../Style/InputBox.css';
+import '../Style/General.css';
+
+class InfoBox extends Component {
+    
+    constructor(props){
+        super(props);
+    } 
+
+
+
+    render() {
+        return (
+            <div className="infoBoxGridPos box">
+                <h1>About the App</h1>
+                <p className="focusField">
+                    dwvazugvwzaufzdwuagwuogawofigawfdbauwbdawugb
+                    dawgbdiagwdugawdfdwvazugvwzaufzdwuagwuogawof
+                    igawfdbauwbdawugbdawgbdiagwdugawdfdwvazugvwz
+                    aufzdwuagwuogawofigawfdbauwbdawugbdawgbdiagw
+                    dugawdfdwvazugvwzaufzdwuagwuogawofigawfdbauw
+                    bdawugbdawgbdiagwdugawdfdwvazugvwzaufzdwuagwu
+                    ogawofigawfdbauwbdawugbdawgbdiagwdugawdfdwvazu
+                    gvwzaufzdwuagwuogawofigawfdbauwbdawugbdawgbdia
+                    dwvazugvwzaufzdwuagwuogawofigawfdbauwbdawugb
+                    dawgbdiagwdugawdfdwvazugvwzaufzdwuagwuogawof
+                    igawfdbauwbdawugbdawgbdiagwdugawdfdwvazugvwz
+                    aufzdwuagwuogawofigawfdbauwbdawugbdawgbdiagw
+                    dugawdfdwvazugvwzaufzdwuagwuogawofigawfdbauw
+                    bdawugbdawgbdiagwdugawdfdwvazugvwzaufzdwuagwu
+                    ogawofigawfdbauwbdawugbdawgbdiagwdugawdfdwvazu
+                    gvwzaufzdwuagwuogawofigawfdbauwbdawugbdawgbdia
+                    dwvazugvwzaufzdwuagwuogawofigawfdbauwbdawugb
+                    dawgbdiagwdugawdfdwvazugvwzaufzdwuagwuogawof
+                    igawfdbauwbdawugbdawgbdiagwdugawdfdwvazugvwz
+                    aufzdwuagwuogawofigawfdbauwbdawugbdawgbdiagw
+                    dugawdfdwvazugvwzaufzdwuagwuogawofigawfdbauw
+                    bdawugbdawgbdiagwdugawdfdwvazugvwzaufzdwuagwu
+                    ogawofigawfdbauwbdawugbdawgbdiagwdugawdfdwvazu
+                    gvwzaufzdwuagwuogawofigawfdbauwbdawugbdawgbdia
+                    gwdugawdf              
+                </p>
+            </div>
+        );
+    }
+}
+
+export default InfoBox;
