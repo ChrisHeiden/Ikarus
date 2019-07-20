a488f2f0fb86bc27ffd4d401ec4ac4f722de1f3b
Christopher <s0549453@htw-berlin.de>
2019-07-20 18:11:41 +0200

    add functions, Components, and states

--- client/src/Components/Transition.js
+++ client/src/Components/Transition.js
@@ -4,17 +4,43 @@ import '../Style/App.css';
 import '../Style/InfoVis.css';
 import InfoVis from './InfoVis';
 import Button from './Button';
+import InputBox from './InputBox';
+import InfoBox from './InfoBox';
 
 class Transition extends Component {
   
   constructor(props) {
     super(props);
+    this.state ={
+      showInfoBox: false,
+      showInputField: false
+    }; 
+    this.showInfoBoxFunc = this.showInfoBoxFunc.bind(this);
+    this.showInputBoxFunc = this.showInputBoxFunc.bind(this);
+
   }
 
-  componentDidMount() {
+
+  showInfoBoxFunc(info){
+    this.setState({ showInfoBox: info });
+  }
+
+  showInputBoxFunc(info){
+    this.setState({ showInputField: info });
   }
 
   render() {
+
+    let showInfoBox;
+    let showInputField;
+
+    if(this.state.showInputField === true){
+      showInputField = <InputBox/>;
+    }
+
+    if(this.state.showInfoBox === true){
+      showInfoBox = <InfoBox/>;
+    }
 
     return (
       <div className="transition generalGrid">
@@ -22,11 +48,15 @@ class Transition extends Component {
           <h1>Climate Change</h1>
         </div>
         <div className="buttonLayout buttonGridPos">
-          <Button content="i"/>
-          <Button content="<"/>
+          <Button content="i" click={this.showInfoBoxFunc}/>
+          <Button content="<" click={this.showInputBoxFunc}/>
         </div>
-        <div className="infoVisGridPos">
+        <div className="mainFocusGridPos mainFocus">
           <InfoVis/>
+          <div className="showBoxes">
+            {showInfoBox}
+            {showInputField}
+          </div>
         </div>
       </div>
     );
