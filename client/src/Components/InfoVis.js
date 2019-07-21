0a2c1f5b53337f3e3b24c6f6fa7cc691f9b0a4bc
Christopher <s0549453@htw-berlin.de>
2019-07-21 18:31:34 +0200

    remove comments

--- client/src/Components/InfoVis.js
+++ client/src/Components/InfoVis.js
@@ -47,7 +47,6 @@ class InfoVis extends Component {
     };
 
     calcDots() {
-//        const numbers = [95, 95, 95, 95, 50, 50, 50, 50, 50, 75, 75, 75, 75, 75,] //max height is 95
         const tweets = this.state.tweets;
         let listItems = tweets.map((tweet, index) =>
             <Dot key={index} angle={index*2} y={this.state.xPos} x={this.state.yPos} radius={this.state.amountOfTweets/2}>{tweet.user.location}></Dot>
@@ -79,43 +78,3 @@ class InfoVis extends Component {
 }
 
 export default InfoVis;
-
-
-/*
-                    <Dot radius={this.radius}  x={this.xPos} y={this.yPos}/>
-
-
-    calcDots() {
-        const numbers = this.state.tweets;
-        const dots = numbers.map((number, index) => {
-            <Dot radius={this.radius} x={this.xPos} y={this.yPos} angle={index}/>
-        });
-        return(dots);
-    }
-
-
-    render() {
-        if(this.state.gotInformation === false)
-        {
-            return(<React.Fragment/>);
-        }
-        else{    
-            let heightDiv = this.state.amountOfTweets * 2;
-            let widthDiv = this.state.amountOfTweets * 2;
-
-            let styles = {
-                width: widthDiv+'px',
-                height: heightDiv+'px',
-            };
-
-            return (
-                <div className="infoVisGridPos dotPos">
-                    <div ref={this.refCallback} className="dot mainDot" style={styles}/>
-                    {this.calcDots()}
-                </div>
-            );
-        }
-    }
-}
-
-*/
