b7471a3083041a17258af442acf684523e530e17
Christopher <s0549453@htw-berlin.de>
2019-08-03 11:05:34 +0200

    add slider component

--- client/src/Components/InputBox.js
+++ client/src/Components/InputBox.js
@@ -1,39 +1,37 @@
 import React, { Component } from 'react';
 import '../Style/InputBox.css';
 import '../Style/General.css';
-
+import Slider from './Slider'
 class InputBox extends Component {
     
     constructor(props){
         super(props);
         this.state = {
-            sliderValue: 0,
             textValue: "",            
         };
 
-        this.handleSlider = this.handleSlider.bind(this);
         this.handleText = this.handleText.bind(this);
     } 
-
-    handleSlider(event){
-        this.setState({sliderValue: event.target.value});
-    }
 
     handleText(event) {
         this.setState({textValue: event.target.value});
     }
 
     render() {
-        //console.log(this.props.amountTw)
-        //console.log(this.state.sliderValue)
+        console.log(this.props.amountTw)
+        console.log(this.state.sliderValue)
         return (
             <div className="box inputBoxGridPos">
                 <h1>Filters</h1>
                 <div className="focusField">
-                    <p>Amount of Locations</p>
-                    <input type="range" min="this.props.amountTw" max={this.props.amountTw} value={this.state.SliderValue} onChange={this.handleSlider}/>
+                    <p>Show how many:</p>
+                    <Slider step={1} min={0} max={100} value={100}/>
+
                     <p>Name of the Locations</p>
                     <input  type="text" value={this.state.textValue} onChange={this.handleText}/>
+                   
+                    <p>How old can it be</p>
+                    <Slider value={0} step={0.1} min={0} max={1}/>
                 </div>
             </div>
         );
