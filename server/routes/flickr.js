63ded0d4265dcc87b895525e7a97860959cc76cb
Christopher <s0549453@htw-berlin.de>
2019-08-22 16:07:03 +0200

    add flickr code

--- server/routes/flickr.js
+++ server/routes/flickr.js
@@ -6,55 +6,34 @@ var tagData = new Data();
 
 
 let dataSet = [];
-
-var Flickr = require("flickrapi"),
-    flickrOptions = {
-      api_key: "061fec8f4fe3345cb146b8b6c0f85608",
-      secret: "554d14d5178835d6"
-    };
-
-Flickr.tokenOnly(flickrOptions, function(error, flickr) {
-    flickr.photos.search({
-        tags: "climateChange"
-      }, function(err, photoSet) {
-        result = photoSet.photos.photo;
-    });
-});
-
 /* GET users listing. */
 router.get('/', function(req, res) {
 
     async function getGeneralData(){
-        var result = [];
-        await axios.get('http://api.flickr.com/services/rest/?method=flickr.photos.search&api_key=061fec8f4fe3345cb146b8b6c0f85608&tags=' + tagData.getTag() + '&format=json&nojsoncallback=1')
-        .then((response) => {
-            let result = response.data.photos.photo;
-        })
-        .catch((err) => {
-    
-        })
+        var result;
+        result = await axios.get('http://api.flickr.com/services/rest/?method=flickr.photos.search&api_key=061fec8f4fe3345cb146b8b6c0f85608&tags=' + tagData.getTag() + '&format=json&nojsoncallback=1')
         return result;
     }
 
     async function getSpecificData(result){
         var flickrSet = [];
 
-        for(let i = 0; i < result.length; ++i)
-        {
-            axios.get(' https://www.flickr.com/services/rest/?method=flickr.photos.getInfo&api_key=061fec8f4fe3345cb146b8b6c0f85608&photo_id=' + result[i].id + '&format=json&nojsoncallback=1')
+        for (const item of result) {
+            axios.get(' https://www.flickr.com/services/rest/?method=flickr.photos.getInfo&api_key=061fec8f4fe3345cb146b8b6c0f85608&photo_id=' + item.id + '&format=json&nojsoncallback=1')
             .then((response) => {
-                flickrSet.push(response.photo);
-            })
-            .catch((err) => {
-
+                flickrSet.push(response.data.photo);
             })
         }
-        return result;
+        return flickrSet;
     }
     
     
-    getSpecificData(getGeneralData());
-    res.json(result);       
+    let result = getGeneralData();
+    result.then((response) => {
+        result = response.data.photos.photo;
+        const flickrSet = getSpecificData(result);
+        res.json(result);       
+    })
 });
 
 module.exports = router;
