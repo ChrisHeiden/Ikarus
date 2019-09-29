d3218ea2b06b30ecf4b5154dea101406550d5f6b
Christopher <s0549453@htw-berlin.de>
2019-09-29 14:03:19 +0200

    remove console.log

--- server/routes/plattformData.js
+++ server/routes/plattformData.js
@@ -1,18 +1,18 @@
 let data = {
     twitter:{
-        consumer_key: '',
-        consumer_secret: '',
-        access_token: '',
-        access_token_secret: '',
+        consumer_key: 'oKqlvjQlzINyw8awCuZ05TN7Z',
+        consumer_secret: 'R4DCTKgP27K9jcu0beBOiilPTD35FO17ZHj7joFqJFyBlRYm0y',
+        access_token: '1122925591274110977-T2Z5tw2ThjnCGvtq9RjcIIGZva6jjD',
+        access_token_secret: 'FcY4MhmIxjAnG58TIlqg1hU887MqXUYGEKS0bgzNEwYC4',
     },
     flickr:{
-        api_key:''
+        api_key:'061fec8f4fe3345cb146b8b6c0f85608'
     },
     tumblr:{
-        consumer_key: '',
-        consumer_secret: '',
-        token: '',
-        token_secret: ''
+        consumer_key: 'mH85E3PedN8n8XluzYfynjryLpPzG7Y6LdtGqTRrCuwmERZV3q',
+        consumer_secret: 'V4imWQtEQGkZeQDOKV9bwPq0Aa5wnA7XAJCZggYVQHgJBxuAzi',
+        token: 'PGoFny6du63E6ttxSNdD27viCF50JgR73cBBERTiF5YW4XUgiT',
+        token_secret: 'y3JeSSl7oZBME7AM7Gdv0ZD3y8AVW7PBf37bib2SSFELeBtyFO'
     }
 }
 
